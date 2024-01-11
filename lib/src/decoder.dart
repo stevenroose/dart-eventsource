fed9ee7a623b5f168632d60f6515156270ffbbab
georgezhang <georgezhang@nureva.com>
2024-01-10 17:08:57 -0700

    passing error from the source stream

--- lib/src/decoder.dart
+++ lib/src/decoder.dart
@@ -23,10 +23,8 @@ class EventSourceDecoder implements StreamTransformer<List<int>, Event> {
       // This stream will receive chunks of data that is not necessarily a
       // single event. So we build events on the fly and broadcast the event as
       // soon as we encounter a double newline, then we start a new one.
-      stream
-          .transform(new Utf8Decoder())
-          .transform(new LineSplitter())
-          .listen((String line) {
+      stream.transform(new Utf8Decoder()).transform(new LineSplitter()).listen(
+          (String line) {
         if (line.isEmpty) {
           // event is done
           // strip ending newline from data
@@ -60,6 +58,8 @@ class EventSourceDecoder implements StreamTransformer<List<int>, Event> {
             retryIndicator?.call(new Duration(milliseconds: int.parse(value)));
             break;
         }
+      }, onError: (error) {
+        controller.addError(error);
       });
     });
     return controller.stream;
